faa1612c227e51eaf8458b9fd286c81669fd3cfb
yomogi-tyannneru <haruto.matsuda@gmail.com>
2023-01-26 10:41:40 +0900

    コントローラー作成

---
+++ stock2/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+
+  /**
+   * ユーザー情報 Service
+   */
+  @Autowired
+  private UserService userService;
+
+  /**
+   * ユーザー情報一覧画面を表示
+   * @param model Model
+   * @return ユーザー情報一覧画面
+   */
+  @GetMapping(value = "/user/list")
+  public String displayList(Model model) {
+    List<User> userlist = userService.searchAll();
+    model.addAttribute("userlist", userlist);
+    return "user/list";
+  }
+
+  /**
+   * ユーザー新規登録画面を表示
+   * @param model Model
+   * @return ユーザー情報一覧画面
+   */
+  @GetMapping(value = "/user/add")
+  public String displayAdd(Model model) {
+    return "user/add";
+  }
+
+  /**
+   * ユーザー情報詳細画面を表示
+   * @param id 表示するユーザーID
+   * @param model Model
+   * @return ユーザー情報詳細画面
+   */
+  @GetMapping("/user/{id}")
+  public String displayView(@PathVariable Long id, Model model) {
+    return "user/view";
+  }
+}
